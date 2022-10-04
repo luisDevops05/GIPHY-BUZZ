5a226c39e8c4c5d76d276583061feec618f7a24a
JlSanchez05 <luisdevops0@gmail.com>
2022-10-03 21:39:40 -0400

    added some logic to it and styling

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('script is running!')
+console.log('script is running!') 
+
+let searchBar = document.getElementById('search') 
+
+let submitBtn = document.getElementById('submit')
+
+
+
+let searchbarHandle = function() {
+
+    
+submitBtn.addEventListener('click',function(){
+    console.log('working')
+let empty = ''
+if(searchBar.value === empty){
+        console.log('please fill out the search')
+        alert('Please Fill Out Search Tab')
+        event.preventDefault()
+    }    
+})
+
+
+
+}
+searchbarHandle()
+ 
+
+
+
