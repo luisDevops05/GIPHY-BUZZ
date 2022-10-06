8ac0bbcbaace5c775fa73d6b3e55e8a61b9d9813
JlSanchez05 <luisdevops0@gmail.com>
2022-10-06 18:08:13 -0400

    added js to fetch images webpage is coming together

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+
+
 console.log('script is running!') 
 
 // global variables
 let searchBar = document.getElementById('search') 
-let submitBtn = document.getElementById('submit')
+let submitBtn = document.getElementById('submit') 
+
+let img1 = document.querySelector('.img1')
+let img2 = document.querySelector('.img2')
+let img3 = document.querySelector('.img3')
+let img4 = document.querySelector('.img4')
 
 
 
@@ -24,6 +31,34 @@ if(searchBar.value === empty){
 // calling function expression
 searchbarHandle()
  
+//start of images being displayed when user is in UI
+const apiUrlTrending4 = 'https://api.giphy.com/v1/gifs/trending?api_key=nhTKuYXaMk6fp1e6S3Ts68zTe4nEmEN0&limit=4'
 
 
+let trendingGiphys = function(){
+    fetch(apiUrlTrending4).then((response)=>{
+        if(response.ok){
+            return response.json()
+        }else{
+            throw new Error('Network response error!')
+        }
+    }).then((data)=>{
+        console.log(data)
+        showTrending(data)
+    }).catch((error)=>console.log('Fetch Error',error))
+}
 
+function showTrending(data){
+    let urlTrending1 = data.data[0].images.downsized.url
+    let urlTrending2 = data.data[1].images.downsized.url
+    let urlTrending3 = data.data[2].images.downsized.url
+    let urlTrending4 = data.data[3].images.downsized.url
+    
+    img1.style.backgroundImage = 'url('+urlTrending1+')'
+    img2.style.backgroundImage = 'url('+urlTrending2+')'
+    img3.style.backgroundImage = 'url('+urlTrending3+')'
+    img4.style.backgroundImage = 'url('+urlTrending4+')'
+    
+}
+
+trendingGiphys()
