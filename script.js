6ad8a8aad4a12b26e1358ae6371e0ac66d5112cf
JlSanchez05 <luisdevops0@gmail.com>
2022-10-13 21:21:59 -0400

    working on bugs

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 
 
 
-console.log('script is running!') 
-
 // global variables
 let searchBar = document.getElementById('search') 
 let submitBtn = document.getElementById('submit') 
-
 
 //these are the trending images
 let img1 = document.querySelector('.img1')
@@ -21,13 +18,48 @@ let img7 = document.querySelector('.imgT3')
 let img8 = document.querySelector('.imgT4') 
 
 
-// setting the userData
+// // setting the userData
+// submitBtn.addEventListener('click',function(){
+//     sessionStorage.setItem('userData',searchBar.value)
+// })
+let imgc1 = document.querySelector('.con1')
+let imgc2 = document.querySelector('.con2')
+let imgc3 = document.querySelector('.con3')
+let imgc4 = document.querySelector('.con4')
+let imgc5 = document.querySelector('.con5')
+let imgc6 = document.querySelector('.con6')
+let imgc7 = document.querySelector('.con7')
+let imgc8 = document.querySelector('.con8')
+let imgc9 = document.querySelector('.con9')
+let imgc10 = document.querySelector('.con10')
+let imgc11 = document.querySelector('.con11')
+let imgc12 = document.querySelector('.con12')
+let imgc13 = document.querySelector('.con13')
+let imgc14 = document.querySelector('.con14')
+let imgc15 = document.querySelector('.con15')
+let imgc16 = document.querySelector('.con16')
+let imgc17 = document.querySelector('.con17')
+let imgc18 = document.querySelector('.con18')
+let imgc19 = document.querySelector('.con19')
+let imgc20 = document.querySelector('.con20')
+let imgc21 = document.querySelector('.con21')
+let imgc22= document.querySelector('.con22')
+let imgc23 = document.querySelector('.con23')
+let imgc24 = document.querySelector('.con24')
+let imgc25 = document.querySelector('.con25')
+
+
+
+
 submitBtn.addEventListener('click',function(){
-    sessionStorage.setItem('userData',searchBar.value)
+    sessionStorage.setItem('userDataRes',searchBar.value)
+    
 })
 
+let userInput = sessionStorage.getItem('userData') 
+let userInputRes = sessionStorage.getItem('userDataRes')
 
-
+console.log(userInputRes)
 
 
 // function preventing user from empty search
@@ -44,11 +76,11 @@ if(searchBar.value === empty){
 })
 
 }
-
 searchbarHandle()
  
 //Api links to trending and stickers giphy
 const apiUrlTrending4 = 'https://api.giphy.com/v1/gifs/trending?api_key=nhTKuYXaMk6fp1e6S3Ts68zTe4nEmEN0&limit=9' 
+
 const apiUrlTrendingS = 'https://api.giphy.com/v1/stickers/trending?api_key=nhTKuYXaMk6fp1e6S3Ts68zTe4nEmEN0&limit=8' // stickers
 
 
@@ -109,8 +141,6 @@ function showTrendingStickers(data){
     img7.style.backgroundImage = 'url('+urlTrendingS3+')'
     img8.style.backgroundImage = 'url('+urlTrendingS4+')'
 } 
-
-
 
 
 trendingGiphys()
